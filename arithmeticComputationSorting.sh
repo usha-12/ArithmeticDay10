333320619b2a66483aeaec0df4d2cde22bbb9cf7
usha ahirwar <66367304+usha-12@users.noreply.github.com>
2022-08-04 11:32:25 +0530

    Update arithmeticComputationSorting.sh

--- arithmeticComputationSorting.sh
+++ arithmeticComputationSorting.sh
@@ -1,4 +1,18 @@
+#Start with Displaying Welcome to Employee Wage Computation Program on Master #Branch
+
+
 echo ""
 echo "Welcome to the Arithmetic Computation and Sorting Program"
 echo "This program computes different arithmetic expressions and sorts the results."
+echo ""
+
+#Write program to take three inputs – a, b & c
+echo -n " Enter the first number : "
+read n1
+echo -n " Enter the second number : "
+read n2
+echo -n " Enter the third number : "
+read n3
+
+echo "You entered $n1 $n2 $n3"
 echo ""
